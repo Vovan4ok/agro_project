f6068838a7bca74fa1ec8ae95c8d737cb369f648
vovan4ok <bosik_ck@ukr.net>
2024-03-15 18:45:48 +0300

    Initial commit

---
+++ src/main/java/com/agro/agro_project/AgroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.agro.agro_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agro/agro_project/AgroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agro.agro_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
